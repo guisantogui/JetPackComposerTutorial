b3bf08fe85c67e63f5d20247e8d80753a0ee2831
makadu <makaduapp@outlook.com>
2023-08-07 08:41:03 -0300

    Adicionando elementos de texto e coluna

--- app/src/main/java/com/example/jetpackcomposertutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposertutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposertutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,18 +14,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            MessageCard("Android 2023!!")
+            MessageCard(Message("Android 2023", "BUSQUEM CONHECIMENTO"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String){
-    return Text(text = "Hello $name")
+fun MessageCard(message: Message){
+    Column {
+        Text(text = "Hello ${message.author}")
+        Text(text = message.body )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard(){
-    MessageCard("Android")
+    MessageCard(Message("Android 2023", "BUSQUEM CONHECIMENTO"))
 }
