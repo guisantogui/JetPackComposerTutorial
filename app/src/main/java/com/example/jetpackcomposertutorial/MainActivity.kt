d941d57e3e44c31a8ec7889f8300158f6d623dc4
makadu <makaduapp@outlook.com>
2023-08-08 08:36:56 -0300

    Tutorial: https://developer.android.com/jetpack/compose/tutorial finalizado

--- app/src/main/java/com/example/jetpackcomposertutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposertutorial/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,11 +16,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -30,10 +39,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
             JetPackComposerTutorialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android 2023", "BUSQUEM CONHECIMENTO"))
+                    Conversation(messages = SampleData.conversationSample)
+                    // MessageCard(Message("Android 2023", "BUSQUEM CONHECIMENTO"))
                 }
             }
         }
@@ -43,43 +53,84 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(message: Message){
+fun MessageCard(message: Message) {
     Row {
 
-        Image ( painter = painterResource(id = R.drawable.me),
-                contentDescription = "User profile picture",
+        Image(
+            painter = painterResource(id = R.drawable.me),
+            contentDescription = "User profile picture",
 
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-                    .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
 
         )
-        Spacer (modifier = Modifier.width(10.dp))
-        Column {
-            Text(   text = "Hello ${message.author}",
-                    color = MaterialTheme.colorScheme.secondary,
-                    style = MaterialTheme.typography.titleMedium
-                )
-            Text(   text = message.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.bodySmall
+        Spacer(modifier = Modifier.width(10.dp))
 
+        //Começado, falta terminar o capítulo
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
+            //Gerava warning sem o parametro label
+            label = ""
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = "Hello ${message.author}",
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleLarge
             )
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+            MessageCard(message = it)
         }
     }
 }
 
 @Preview(name = "Light Mode")
-@Preview(   uiMode = Configuration.UI_MODE_NIGHT_YES,
-            showBackground = true,
-            name = "Dark Mode"
-        )
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun PreviewMessageCard(){
+fun PreviewMessageCard() {
     JetPackComposerTutorialTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
             MessageCard(Message("Android 2023", "BUSQUEM CONHECIMENTO"))
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetPackComposerTutorialTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
